c1064962492da0ab755265d0d6b7c893614a89ac
cepta <babaseyidara@gmail.com>
2025-09-27 12:50:17 -0500

    Add project files.

---
+++ Controllers/ProfilesController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProfileManager.Models;
+
+namespace ProfileManager.Controllers
+{
+    public class ProfilesController : Controller
+    {
+        // GET: ProfilesController
+        public ActionResult Index()
+        {
+            var profiles = Profile.profiles;
+            return View(profiles);
+        }
+
+        // GET: ProfilesController/Details/5
+        public ActionResult Details(int id)
+        {
+            var profile = Profile.profiles.FirstOrDefault(p => p.Id == id);
+            return View(profile);
+        }
+
+        // GET: ProfilesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProfilesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Profile model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    model.Id = Profile.profiles.Max(p => p.Id) + 1;
+                    Profile.profiles.Add(model);
+                    return RedirectToAction(nameof(Index));
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProfilesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var profile = Profile.profiles.FirstOrDefault(p => p.Id == id);
+            return View(profile);
+        }
+
+        // POST: ProfilesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Profile model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var profile = Profile.profiles.FirstOrDefault(p => p.Id == id);
+                    if (profile != null)
+                    {
+                        profile.FirstName = model.FirstName;
+                        profile.LastName = model.LastName;
+                        profile.Email = model.Email;
+                        profile.Phone = model.Phone;
+                        profile.Address = model.Address;
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProfilesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProfilesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Profile model)
+        {
+            try
+            {
+                var profile = Profile.profiles.FirstOrDefault(p => p.Id == id);
+                if (profile != null)
+                {
+                    Profile.profiles.Remove(profile);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Profile.cs
@@ -0,0 +1,18 @@
+﻿namespace ProfileManager.Models
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+        public static List<Profile> profiles =
+        [
+            new Profile { Id = 1, FirstName = "Ade", LastName = "Tolu", Email = "ade@gmail.com", Phone = "08012345678", Address = "Lagos, Nigeria" },
+            new Profile { Id = 2, FirstName = "John", LastName = "Paul", Email = "john@gmail.com", Phone = "08087654321", Address = "Abuja, Nigeria" }
+        ];
+    }
+}
