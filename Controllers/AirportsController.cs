c205a8ac40a4e004084721b8a3904f10cc19d60a
cepta <babaseyidara@gmail.com>
2025-10-07 05:43:37 -0500

    Added Airports

---
+++ Controllers/AirportsController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProfileManager.Models;
+using System.Reflection;
+
+namespace ProfileManager.Controllers
+{
+    public class AirportsController : Controller
+    {
+        // GET: AirportsController
+        public ActionResult Index()
+        {
+            var airports = Airport.airports;
+            return View(airports);
+        }
+
+        // GET: AirportsController/Details/5
+        public ActionResult Details(int id)
+        {
+            var airport = Airport.airports.FirstOrDefault(p => p.Id == id);
+            return View(airport);
+        }
+
+        // GET: AirportsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AirportsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Airport model)
+        {
+            try
+            {
+                model.Id = Airport.airports.Max(p => p.Id) + 1;
+                Airport.airports.Add(model);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AirportsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var airport = Airport.airports.FirstOrDefault(p => p.Id == id);
+            return View(airport);
+        }
+
+        // POST: AirportsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Airport model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var airport = Airport.airports.FirstOrDefault(p => p.Id == id);
+                    if (airport != null)
+                    {
+                        airport.Name = model.Name;
+                        airport.IATA = model.IATA;
+                        airport.ICAO = model.ICAO;
+                        airport.City = model.City;
+                        airport.Country = model.Country;
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(model);
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AirportsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AirportsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                var airport = Airport.airports.FirstOrDefault(p => p.Id == id);
+                if (airport != null)
+                {
+                    Airport.airports.Remove(airport);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Airport.cs
@@ -0,0 +1,19 @@
+﻿namespace ProfileManager.Models
+{
+    public class Airport
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string IATA { get; set; }
+        public string ICAO { get; set; }
+        public static List<Airport> airports = new List<Airport>
+        {
+            new Airport { Id = 1, Name = "Hartsfield-Jackson Atlanta International Airport", City = "Atlanta", Country = "United States", IATA = "ATL", ICAO = "KATL" },
+            new Airport { Id = 2, Name = "Beijing Capital International Airport", City = "Beijing", Country = "China", IATA = "PEK", ICAO = "ZBAA" },
+            new Airport { Id = 3, Name = "Dubai International Airport", City = "Dubai", Country = "United Arab Emirates", IATA = "DXB", ICAO = "OMDB"}
+
+         };
+    }
+}
